7cf246f20687a20491b4cd7086f299d611c9948f
skwig <skwig@users.noreply.github.com>
2025-05-06 20:35:57 +0200

    fix icon, remove nss

--- pkgs/by-name/fr/freelens/linux.nix
+++ pkgs/by-name/fr/freelens/linux.nix
@@ -11,11 +11,9 @@ in appimageTools.wrapType2 {
     wrapProgram $out/bin/${pname} \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-    install -m 444 -D ${appimageContents}/usr/sharKubernetis IDEe/icons/hicolor/512x512/apps/${pname}.png \
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/${pname}.png \
        $out/share/icons/hicolor/512x512/apps/${pname}.png
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
   '';
-
-  extraPkgs = pkgs: [ pkgs.nss_latest ];
 }
