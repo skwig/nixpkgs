93b0120b1d240bfef77b258a86494ab5bf7fd33c
skwig <16136203+skwig@users.noreply.github.com>
2026-06-01 22:34:38 +0200

    use concrete ref

--- pkgs/by-name/hy/hyprland/package.nix
+++ pkgs/by-name/hy/hyprland/package.nix
@@ -88,8 +88,9 @@ customStdenv.mkDerivation (finalAttrs: {
     owner = "hyprwm";
     repo = "hyprland";
     fetchSubmodules = true;
-    # version: bump to 0.54.2
-    rev = "59f9f2688ac508a0584d1462151195a6c4992f99";
+    ref = "v0.54.2";
+    # # version: bump to 0.54.2
+    # rev = "59f9f2688ac508a0584d1462151195a6c4992f99";
     hash = "sha256-YF31zdrg7ctjD2CR+8rzKNsR0luYgoq8lhRkzd6myL8=";
     # hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
   };
