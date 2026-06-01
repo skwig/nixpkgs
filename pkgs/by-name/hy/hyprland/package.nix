10b750c421e9deba28df0f1ae1f4e40cd8099566
skwig <16136203+skwig@users.noreply.github.com>
2026-06-01 23:53:46 +0200

    hashmagic

--- pkgs/by-name/hy/hyprland/package.nix
+++ pkgs/by-name/hy/hyprland/package.nix
@@ -88,7 +88,8 @@ customStdenv.mkDerivation (finalAttrs: {
     fetchSubmodules = true;
     tag = "v0.54.3";
     # rev = "521ece463c4a9d3d128670688a34756805a4328f";
-    hash = "sha256-e+mVjQL3V+xoaH1c3YqAzRq9wwiuEYQTOgZlK0LwfYA=";
+    # hash = "sha256-e+mVjQL3V+xoaH1c3YqAzRq9wwiuEYQTOgZlK0LwfYA=";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
   };
 
   postPatch = ''
